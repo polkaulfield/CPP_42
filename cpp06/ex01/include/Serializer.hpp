7bd6332dda8be2277d929489857f8b9a65dde1e6
Pol Kaulfield <mr.tamuraa@gmail.com>
2025-08-05 18:07:31 +0200

    Fixed hpp

--- cpp06/ex01/include/Serializer.hpp
+++ cpp06/ex01/include/Serializer.hpp
@@ -4,9 +4,6 @@
 #include <bits/types.h>
 #include "Data.hpp"
 
-// This type is missing from cpp98..
-typedef unsigned long long uintptr_t;
-
 using std::string;
 
 class Serializer {
@@ -14,7 +11,7 @@ private:
   Serializer(void);
   Serializer(const Serializer &serializer);
   const Serializer &operator=(const Serializer &serializer);
-  virtual ~Serializer() = 0;
+  ~Serializer();
 
 public:
   static uintptr_t serialize(Data* ptr);
